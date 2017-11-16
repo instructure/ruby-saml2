aeb42d549dbfb54b0dffc782f2cb5f3867cf7b00
Cody Cutrer <cody@instructure.com>
2017-11-16 14:35:21 -0700

    implement HTTP POST binding
    no signature verification yet
    
    Change-Id: I9d6df2c8d04b5e7e8b79c0c2e4172be1d929d299
    Reviewed-on: https://gerrit.instructure.com/133097
    Tested-by: Jenkins
    Reviewed-by: Rob Orton <rob@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>

--- lib/saml2/bindings/http_post.rb
+++ lib/saml2/bindings/http_post.rb
@@ -1,7 +1,35 @@
+require 'base64'
+
 module SAML2
   module Bindings
     module HTTP_POST
       URN ="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST".freeze
+
+      class << self
+        def decode(post_params)
+          base64 = post_params['SAMLRequest'] || post_params['SAMLResponse']
+          raise MissingMessage unless base64
+
+          raise MessageTooLarge if base64.bytesize > SAML2.config[:max_message_size]
+
+          xml = begin
+            Base64.decode64(base64)
+          rescue ArgumentError
+            raise CorruptMessage
+          end
+
+          message = Message.parse(xml)
+          [message, post_params['RelayState']]
+        end
+
+        def encode(message, relay_state: nil)
+          xml = message.to_s(pretty: false)
+          key = message.is_a?(Request) ? 'SAMLRequest' : 'SAMLResponse'
+          post_params = { key => Base64.encode64(xml) }
+          post_params['RelayState'] = relay_state if relay_state
+          post_params
+        end
+      end
     end
   end
 end
