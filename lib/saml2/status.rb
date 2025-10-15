21ab21bab7d22e8b30248a08e6d8190a44ccafac
Cody Cutrer <cody@instructure.com>
2025-10-15 16:49:20 -0600

    add support for nested status codes
    Change-Id: I5564f849d2d792a60fe97a601a28608c86d84984
    Reviewed-on: https://gerrit.instructure.com/c/ruby-saml2/+/391738
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Michael Hargiss <mhargiss@instructure.com>
    QA-Review: Cody Cutrer <cody@instructure.com>
    Product-Review: Cody Cutrer <cody@instructure.com>

--- lib/saml2/status.rb
+++ lib/saml2/status.rb
@@ -7,24 +7,74 @@ module SAML2
     SUCCESS   = "urn:oasis:names:tc:SAML:2.0:status:Success"
     REQUESTER = "urn:oasis:names:tc:SAML:2.0:status:Requester"
     RESPONDER = "urn:oasis:names:tc:SAML:2.0:status:Responder"
+    VERSION_MISMATCH = "urn:oasis:names:tc:SAML:2.0:status:VersionMismatch"
 
+    AUTHN_FAILED = "urn:oasis:names:tc:SAML:2.0:status:AuthnFailed"
+    INVALID_ATTR_NAME_OR_VALUE = "urn:oasis:names:tc:SAML:2.0:status:InvalidAttrNameOrValue"
+    INVALID_NAME_ID_POLICY = "urn:oasis:names:tc:SAML:2.0:status:InvalidNameIDPolicy"
+    NO_AUTHN_CONTEXT = "urn:oasis:names:tc:SAML:2.0:status:NoAuthnContext"
+    NO_AVAILABLE_IDP = "urn:oasis:names:tc:SAML:2.0:status:NoAvailableIDP"
+    NO_PASSIVE = "urn:oasis:names:tc:SAML:2.0:status:NoPassive"
+    NO_SUPPORTED_IDP = "urn:oasis:names:tc:SAML:2.0:status:NoSupportedIDP"
+    PARTIAL_LOGOUT = "urn:oasis:names:tc:SAML:2.0:status:PartialLogout"
+    PROXY_COUNT_EXCEEDED = "urn:oasis:names:tc:SAML:2.0:status:ProxyCountExceeded"
+    REQUEST_DENIED = "urn:oasis:names:tc:SAML:2.0:status:RequestDenied"
+    REQUEST_UNSUPPORTED = "urn:oasis:names:tc:SAML:2.0:status:RequestUnsupported"
+    REQUEST_VERSION_DEPRECATED = "urn:oasis:names:tc:SAML:2.0:status:RequestVersionDeprecated"
+    REQUEST_VERSION_TOO_HIGH = "urn:oasis:names:tc:SAML:2.0:status:RequestVersionTooHigh"
+    REQUEST_VERSION_TOO_LOW = "urn:oasis:names:tc:SAML:2.0:status:RequestVersionTooLow"
+    RESOURCE_NOT_RECOGNIZED = "urn:oasis:names:tc:SAML:2.0:status:ResourceNotRecognized"
+    TOO_MANY_RESPONSES = "urn:oasis:names:tc:SAML::2.0:status:TooManyResponses"
+    UNKNOWN_ATTR_PROFILE = "urn:oasis:names:tc:SAML:2.0:status:UnknownAttrProfile"
+    UNKNOWN_PRINCIPAL = "urn:oasis:names:tc:SAML:2.0:status:UnknownPrincipal"
+    UNSUPPORTED_BINDING = "urn:oasis:names:tc:SAML:2.0:status:UnsupportedBinding"
+
+    TOP_LEVEL_STATUS_CODES = [SUCCESS, REQUESTER, RESPONDER, VERSION_MISMATCH].freeze
+
+    # @return [Array<String>]
+    attr_reader :codes
     # @return [String]
-    attr_accessor :code, :message
+    attr_accessor :message, :detail
 
     # @param code [String]
     # @param message [String, nil]
-    def initialize(code = SUCCESS, message = nil)
+    def initialize(code = SUCCESS, message = nil, detail = nil)
       super()
-      @code = code
+      self.codes = code
       @message = message
+      @detail = detail
     end
 
     # (see Base#from_xml)
     def from_xml(node)
       super
-      self.code = node.at_xpath("samlp:StatusCode", Namespaces::ALL)["Value"]
-      self.message = load_string_array(xml, "samlp:StatusMessage")
+
+      @codes.clear
+      code_node = node
+
+      loop do
+        code_node = code_node.at_xpath("samlp:StatusCode", Namespaces::ALL)
+        break unless code_node
+
+        codes << code_node["Value"]
+      end
+      self.message = xml.at_xpath("samlp:StatusMessage", Namespaces::ALL)&.content&.strip
+      self.detail = xml.at_xpath("samlp:StatusDetail", Namespaces::ALL)&.content&.strip
     end
+
+    def code
+      codes.first
+    end
+
+    def codes=(value)
+      codes = Array.wrap(value)
+      unless TOP_LEVEL_STATUS_CODES.include?(codes.first)
+        raise ArgumentError, "Invalid top level status code #{codes.first.inspect}"
+      end
+
+      @codes = codes
+    end
+    alias_method :code=, :codes=
 
     def success?
       code == SUCCESS
@@ -33,10 +83,20 @@ module SAML2
     # (see Base#build)
     def build(builder)
       builder["samlp"].Status do |status|
-        status["samlp"].StatusCode(Value: code)
-        Array(message).each do |m|
-          status["samlp"].StatusMessage(m)
-        end
+        build_code(status, codes, 0)
+
+        status["samlp"].StatusMessage(message) if message
+        status["samlp"].StatusDetail(detail) if detail
+      end
+    end
+
+    private
+
+    def build_code(builder, codes, idx)
+      return if idx >= codes.length
+
+      builder["samlp"].StatusCode(Value: codes[idx]) do |code_builder|
+        build_code(code_builder, codes, idx + 1)
       end
     end
   end
--- spec/lib/saml2/logout_response_spec.rb
+++ spec/lib/saml2/logout_response_spec.rb
@@ -16,6 +16,19 @@ module SAML2
       LogoutResponse.respond_to(logout_request, idp, NameID.new("issuer2"))
     end
 
+    let(:multicode_xml) do
+      <<~XML
+        <?xml version="1.0"?>
+        <samlp:LogoutResponse xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol" xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" ID="_f93ca540-f984-4eaa-b311-2b18a0359f25" Version="2.0" IssueInstant="2025-10-15T22:44:56Z">
+          <samlp:Status>
+            <samlp:StatusCode Value="urn:oasis:names:tc:SAML:2.0:status:Requester">
+              <samlp:StatusCode Value="urn:oasis:names:tc:SAML:2.0:status:InvalidNameIDPolicy"/>
+            </samlp:StatusCode>
+          </samlp:Status>
+        </samlp:LogoutResponse>
+      XML
+    end
+
     it "should generate valid XML" do
       xml = logout_response.to_s
       expect(Schemas.protocol.validate(Nokogiri::XML(xml))).to eq []
@@ -26,6 +39,22 @@ module SAML2
       new_response = LogoutResponse.parse(logout_response.to_s)
       expect(new_response.issuer.id).to eq "issuer2"
       expect(new_response.status.code).to eq Status::SUCCESS
+    end
+
+    it "parses nested codes" do
+      new_response = LogoutResponse.parse(multicode_xml)
+      expect(new_response.status.codes).to eql [Status::REQUESTER, Status::INVALID_NAME_ID_POLICY]
+    end
+
+    def strip_id_and_issue_instant(xml)
+      xml.sub(/ ID="_[a-z0-9\-]+"/, "")
+         .sub(/ IssueInstant="[0-9TZ:\-]+"/, "")
+    end
+
+    it "serializes nested codes" do
+      logout_response = LogoutResponse.new
+      logout_response.status.codes = [Status::REQUESTER, Status::INVALID_NAME_ID_POLICY]
+      expect(strip_id_and_issue_instant(logout_response.to_s)).to eql strip_id_and_issue_instant(multicode_xml)
     end
   end
 end
